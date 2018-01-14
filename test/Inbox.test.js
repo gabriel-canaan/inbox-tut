87d9be59371873333b82e3398002d19dcd11c333
gabriel <777canaan@gmail.com>
2018-01-14 17:57:41 +1300

    deploy to ganache with test pass

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,17 +2,22 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
-  accounts = await web3.eth.getAccounts()
+  accounts = await web3.eth.getAccounts();
 
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+  .deploy({ data: bytecode, arguments: ['dave!'] })
+  .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('inbox', () => {
   it ('deploys a contract', () => {
-    console.log(accounts);
+    console.log(inbox);
   })
 })
 
