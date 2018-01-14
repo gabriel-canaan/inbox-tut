5595bb904b052743e53598b79e5e8a44d3c4cc67
gabriel <777canaan@gmail.com>
2018-01-14 21:26:07 +1300

    test deploy has address  pass

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,21 +1,32 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+const web3 = new Web3(provider();
+
 const {interface, bytecode} = require('../compile');
 
 let accounts;
 let inbox;
 
+
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
 
-  inbox = await new web3.eth.Contract(JSON.parse(interface)).deploy({data: bytecode, arguments: ['dave!']}).send({from: accounts[0], gas: '1000000'});
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+  .deploy({data: bytecode, arguments: ['dave!']})
+  .send({from: accounts[0], gas: '1000000'});
+  inbox.setProvider(provider);
 });
 
 describe('inbox', () => {
   it('deploys a contract', () => {
     assert.ok(inbox.options.address);
+  });
+  it('has a default msssage', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'dave!');
   });
 });
 
