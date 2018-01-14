ae8bf2ddd35934431e61f3a44db16c33f4862b60
gabriel <777canaan@gmail.com>
2018-01-14 17:35:40 +1300

    comment out example tests and log ganache accounts successfully

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,19 +3,76 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-
-class Car {
-  park() {
-    return 'stopped';
-  }
-  drive() {
-    return 'vroom';
-  }
-}
-
-describe('Car', () => {
-  it('can park', () => {
-    const car = new Car();
-    assert.equal(car.park(), 'stopped');
-  });
+beforeEach(() => {
+  web3.eth.getAccounts()
+    .then(fetchedAccounts => {
+      console.log(fetchedAccounts);
+    });
 });
+
+describe('inbox', () => {
+  it ('deploys a contract', () => {
+    
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Car {
+//   park() {
+//     return 'stopped';
+//   }
+//   drive() {
+//     return 'vroom';
+//   }
+// }
+//
+// let car;
+//
+// beforeEach(() => {
+//   car = new Car();
+// })
+//
+// describe('Car', () => {
+//   it('can park', () => {
+//     assert.equal(car.park(), 'stopped');
+//   });
+//   it('can drive', () => {
+//     assert.equal(car.drive(), 'vroom');
+//   });
+// });
